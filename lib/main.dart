8b5c583a6908ea3b19f6e5366880d82e08fb0705
Disaru Gunawardhana <disaru1314@gmail.com>
2024-07-04 12:09:44 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'splash_screen.dart'; // Adjust the import path if needed
 import 'firebase_options.dart';
+import 'user_provider.dart'; // Import the UserProvider
+import 'login_screen.dart'; // Import the login screen
+import 'home_screen.dart'; // Import the home screen
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,18 +18,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'RDB Tickets',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blue,
-      ),
-      home: const SplashScreen(
-        companyLogoPath: 'lib/assets/rdbbanklogo.jpg',
-        companyNames: ['RDB Tickets'],
+    return ChangeNotifierProvider(
+      create: (_) => UserProvider(),
+      child: MaterialApp(
+        title: 'RDB Tickets',
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blue,
+        ),
+        home: const SplashScreen(
+          companyLogoPath: 'lib/assets/rdbbanklogo.jpg',
+          companyNames: ['RDB Tickets'],
+        ),
+        routes: {
+          '/login': (context) => const LoginScreen(),
+          '/home': (context) => const HomeScreen(),
+        },
       ),
     );
   }
 }
+
 
 // More updates should be done 
