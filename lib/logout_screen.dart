e7c8d612437f7c4bb236ec3c26e02f57986d9ce2
Disaru Gunawardhana <disaru1314@gmail.com>
2024-07-08 15:29:29 +0530

    Update logout_screen.dart

--- lib/logout_screen.dart
+++ lib/logout_screen.dart
@@ -39,9 +39,6 @@ class LogoutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Logout'),
-      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () => _signOut(context),
