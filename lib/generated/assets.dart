66d10c4102fd40e01dabeb69cd030f966b0ce59d
Disaru Gunawardhana <disaru1314@gmail.com>
2024-08-11 14:32:33 +0530

    Update assets.dart

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -15,6 +15,8 @@ class Assets {
   static const String libMain = 'lib/main.dart';
   static const String libSplashScreen = 'lib/splash_screen.dart';
   static const String libTaskCreationScreen = 'lib/task_creation_screen.dart';
+  static const String libTaskDetailsScreen = 'lib/task_details_screen.dart';
+  static const String libTaskScreenDelegate = 'lib/task_screen_delegate.dart';
   static const String libTasksListScreen = 'lib/tasks_list_screen.dart';
   static const String libUserProvider = 'lib/user_provider.dart';
 
