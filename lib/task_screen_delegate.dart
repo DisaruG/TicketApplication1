911f3ba9acc007e3549f3482669084d7a0a6d16d
Disaru Gunawardhana <disaru1314@gmail.com>
2024-08-11 14:32:36 +0530

    Create task_screen_delegate.dart

---
+++ lib/task_screen_delegate.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'tasks_list_screen.dart';
+
+class TicketSearchDelegate extends SearchDelegate<String> {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = ''; // Clear the search query
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        Navigator.pop(context); // Close the search page
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return _buildSearchResults(context);
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return _buildSearchResults(context);
+  }
+
+  Widget _buildSearchResults(BuildContext context) {
+    return TasksListScreen(searchQuery: query);
+  }
+}
