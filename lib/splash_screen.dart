742cf4af0bce5d05361a0e3a14d88669f3d7015a
Disaru Gunawardhana <disaru1314@gmail.com>
2024-09-10 19:00:21 +0530

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'home_screen.dart';
 import 'login_screen.dart';
@@ -12,13 +13,15 @@ class SplashScreen extends StatefulWidget {
   });
 
   @override
-  CustomNavigationState createState() => CustomNavigationState();
+  SplashScreenState createState() => SplashScreenState();
 }
 
-class CustomNavigationState extends State<SplashScreen>
-    with TickerProviderStateMixin {
-  late AnimationController _controller;
+class SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+  late AnimationController _backgroundController;
   late Animation<Color?> _backgroundAnimation;
+  late AnimationController _nameController;
+  late AnimationController _loadingController;
+  late Animation<double> _loadingAnimation;
 
   int _currentCompanyNameIndex = 0;
 
@@ -26,23 +29,40 @@ class CustomNavigationState extends State<SplashScreen>
   void initState() {
     super.initState();
     _initializeAnimations();
-    _controller.forward().then((_) => _startNameTransition());
+    _backgroundController.forward().then((_) => _startNameTransition());
   }
 
   void _initializeAnimations() {
-    _controller = AnimationController(
+    _backgroundController = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 3),
+      duration: const Duration(seconds: 5), // Shorter duration for background animation
+    )..repeat(reverse: true);
+
+    _backgroundAnimation = ColorTween(
+      begin: Colors.purple.shade600,
+      end: Colors.blue.shade500,
+    ).animate(
+      CurvedAnimation(
+        parent: _backgroundController,
+        curve: Curves.linear,
+      ),
+    );
+
+    _nameController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800), // Shorter duration for name animation
     );
 
 
-    _backgroundAnimation = ColorTween(
-      begin: Colors.blue.shade700,
-      end: Colors.blue.shade300,
-    ).animate(
+    _loadingController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500), // Shorter duration for loading animation
+    )..repeat();
+
+    _loadingAnimation = Tween<double>(begin: 0, end: 1).animate(
       CurvedAnimation(
-        parent: _controller,
-        curve: Curves.linear,
+        parent: _loadingController,
+        curve: Curves.easeInOut,
       ),
     );
   }
@@ -53,11 +73,15 @@ class CustomNavigationState extends State<SplashScreen>
         _currentCompanyNameIndex =
             (_currentCompanyNameIndex + 1) % widget.companyNames.length;
       });
-      if (_currentCompanyNameIndex < widget.companyNames.length - 1) {
-        _startNameTransition();
-      } else {
-        _checkUserAuthentication();
-      }
+      _nameController.forward().then((_) {
+        _nameController.reverse().then((_) {
+          if (_currentCompanyNameIndex < widget.companyNames.length - 1) {
+            _startNameTransition();
+          } else {
+            _checkUserAuthentication();
+          }
+        });
+      });
     });
   }
 
@@ -84,57 +108,83 @@ class CustomNavigationState extends State<SplashScreen>
 
   @override
   void dispose() {
-    _controller.dispose();
+    _backgroundController.dispose();
+    _nameController.dispose();
+    _loadingController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue, // Set the plain color here
       body: AnimatedBuilder(
         animation: _backgroundAnimation,
         builder: (context, child) {
           return Container(
-            color: _backgroundAnimation.value, // Use plain color instead of gradient
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [_backgroundAnimation.value!, Colors.blue.shade100],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: const [0.0, 1.0],
+              ),
+            ),
             child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const SizedBox(height: 30.0),
-
-                  // Animated company names
-                  AnimatedSwitcher(
-                    duration: const Duration(seconds: 1),
-                    child: Text(
-                      widget.companyNames[_currentCompanyNameIndex],
-                      key: ValueKey<int>(_currentCompanyNameIndex),
-                      style: const TextStyle(
-                        fontSize: 36.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Arial',
-                        color: Colors.white,
-                        shadows: [
-                          Shadow(
-                            blurRadius: 10.0,
-                            color: Colors.black54,
-                            offset: Offset(3.0, 3.0),
-                          ),
-                        ],
+                  // Animated company names with sliding effect
+                  SlideTransition(
+                    position: Tween<Offset>(
+                      begin: const Offset(0, -0.5),
+                      end: Offset.zero,
+                    ).animate(_nameController),
+                    child: AnimatedSwitcher(
+                      duration: const Duration(milliseconds: 300), // Faster transition for company names
+                      child: Text(
+                        widget.companyNames[_currentCompanyNameIndex],
+                        key: ValueKey<int>(_currentCompanyNameIndex),
+                        style: const TextStyle(
+                          fontSize: 36.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 10.0,
+                              color: Colors.black54,
+                              offset: Offset(3.0, 3.0),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 80.0),
+                  const SizedBox(height: 60.0), // Adjusted spacing
 
-                  // Custom Loading Indicator (Smaller)
-                  const SizedBox(
-                    width: 40.0,
-                    height: 40.0,
-                    child: CircularProgressIndicator(
-                      strokeWidth: 4.0,
-                      valueColor:
-                      AlwaysStoppedAnimation<Color>(Colors.white),
-                      backgroundColor: Colors.black26,
+                  // Custom iOS-style Loading Animation
+                  SizedBox(
+                    width: 60.0,
+                    height: 60.0,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        CircularProgressIndicator(
+                          value: _loadingAnimation.value,
+                          strokeWidth: 6.0,
+                          valueColor: const AlwaysStoppedAnimation<Color>(Colors.white),
+                          backgroundColor: Colors.black26,
+                        ),
+                        Positioned(
+                          child: RotationTransition(
+                            turns: _loadingController,
+                            child: const Icon(
+                              CupertinoIcons.refresh,
+                              color: Colors.white,
+                              size: 30.0,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
